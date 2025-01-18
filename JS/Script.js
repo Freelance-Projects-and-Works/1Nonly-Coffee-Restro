12f76ccffcc852d8c5c8b4da396061051633c7a2
1NonlyRohan <rohanjaiswal93774@gmail.com>
2025-01-18 11:42:02 +0530

    Submit: 1Nonly Coffee Restro

--- JS/Script.js
+++ JS/Script.js
@@ -11,32 +11,16 @@ menuCloseButton.addEventListener("click", () => menuOpenButton.click());
 navbarLinks.forEach((link) => {
     link.addEventListener("click", () => menuOpenButton.click());
 });
-/* Initializing Swiper */
-let swiper = new Swiper(".slider-wrapper", {
-    loop: true,
-    grabCursor: true,
-    spaceBetween: 25,
-    // Pagination bullets
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-        dynamicBullets: true,
-    },
-    // Navigation arrows
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    /* Responsive breakpoints */
-    breakpoints: {
-        0: {
-            slidesPerView: 1,
-        },
-        768: {
-            slidesPerView: 2,
-        },
-        1024: {
-            slidesPerView: 3,
-        },
-    },
+
+// bottom-to-top
+
+window.addEventListener('scroll', function() {
+    if (window.scrollY > 300) {
+        document.querySelector('#back-to-top').style.display = 'block';
+    } else {
+        document.querySelector('#back-to-top').style.display = 'none';
+    }
+});
+document.querySelector('#back-to-top').addEventListener('click', function() {
+    window.scrollTo({top: 0, behavior:'smooth'});
 });
